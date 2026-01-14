6f6e6f1f2dcf4b9c7f552dd8712436c322602ba0
lorandkelemen <lorandkelemen@users.noreply.github.com>
2026-01-14 21:21:31 +0200

    day8 tasks part 2 Caesar cipher

--- Day8/CaesarCipher.py
+++ Day8/CaesarCipher.py
@@ -1,20 +1,30 @@
+
 def caesar_cipher():
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-    word = input("Enter a word to encrypt: ").lower()
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+    text = input("Enter your message: ").lower()
     shift = int(input("Enter shift number: "))
 
-    encrypted_word = ""
+    result = ""
 
-    for letter in word:
+    for letter in text:
         if letter in alphabet:
             current_position = alphabet.index(letter)
-            new_position = (current_position + shift) % 26
-            encrypted_word += alphabet[new_position]
-        else:
-            encrypted_word += letter
 
-    print(f"Encrypted word: {encrypted_word}")
+            if direction == "encode":
+                new_position = (current_position + shift) % 26
+            elif direction == "decode":
+                new_position = (current_position - shift) % 26
+            else:
+                print("Invalid direction!")
+                return
+
+            result += alphabet[new_position]
+        else:
+            result += letter
+
+    print(f"Result: {result}")
 
 
 caesar_cipher()
